63d6fe429fe6e9a3c3b391170496b1924fcc04e7
Robert Routhier <robrouth92@gmail.com>
2022-04-03 14:18:06 -0700

    third

---
+++ script.js
@@ -0,0 +1,149 @@
+var passwordLength = 8;
+var choice = []
+
+var specialChar = [
+  '`',
+  '~',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '-',
+  '_',
+  '|',
+ ]
+ var lowLetter = [
+   'a',
+   'b',
+   'c',
+   'd',
+   'e',
+   'f',
+   'g',
+   'h',
+   'i',
+   'j',
+   'k',
+   'l',
+   'm',
+   'n',
+   'o',
+   'p',
+   'q',
+   'r',
+   's',
+   't',
+   'u',
+   'v',
+   'w',
+   'x',
+   'y',
+   'z',
+ ]
+ var upLetter = [
+   'A',
+   'B',
+   'C',
+   'D',
+   'E',
+   'F',
+   'G',
+   'H',
+   'I',
+   'J',
+   'K',
+   'L',
+   'M',
+   'N',
+   'O',
+   'P',
+   'Q',
+   'R',
+   'S',
+   'T',
+   'U',
+   'V',
+   'W',
+   'X',
+   'Y',
+   'Z',
+ ]
+ var getNumbers = [
+   
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+ ]
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword)
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var correctPrompt = getPrompt();
+  if (correctPrompt){
+    var newPassword = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = newPassword;
+  } else{
+    passwordText.value = "";
+  }
+}
+
+//Prompts after click
+function generatePassword() {
+  
+  var password = "";
+  for(var i = 0; i < passwordLength; i++){
+    var randomIndex = Math.floor(Math.random() * choice.length);
+    password = password + choice[randomIndex];
+  }
+  return password
+}
+
+//All prompt questions
+function getPrompt(){
+  choice = []
+  passwordLength = parseInt(prompt("How many characters would you like to use? It must be at least 8 and no more than 128."))
+  
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
+    alert("Character length has to be between 8 - 128");
+    return false;
+  }
+  if(confirm("Would you like lowercase letters in your password?")) {
+    choice = choice.concat(lowLetter);
+    
+  }
+  if(confirm("Would you like uppercase letters in your password?")) {
+    choice = choice.concat(upLetter);
+    
+  }
+  if(confirm("Would you like special characters in your password?")) {
+    choice = choice.concat(specialChar);
+    
+  }
+  if(confirm("Would you like numbers in your password?")) {
+    choice = choice.concat(getNumbers);
+    
+  }
+  return true;
+}
+
